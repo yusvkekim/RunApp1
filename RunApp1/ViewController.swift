e44b343df668b03fced2a5fbd2cf91f1e717bda7
eugene <planetaryg0812@icloud.com>
2020-08-05 20:31:38 +0900

    Initial Commit

---
+++ RunApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunApp1
+//
+//  Created by y kimur on 2020/08/05.
+//  Copyright © 2020 y kimur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
