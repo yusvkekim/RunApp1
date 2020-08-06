2a8106224e2728deb80e76b1c0b3632d896d3e70
eugene <planetaryg0812@icloud.com>
2020-08-06 18:08:42 +0900

    Second commit

---
+++ Pods/Target Support Files/Pods-RunApp1/Pods-RunApp1-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RunApp1 : NSObject
+@end
+@implementation PodsDummy_Pods_RunApp1
+@end
--- RunApp1/ViewController.swift
+++ RunApp1/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import UICircularProgressRing
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var timerRing: UICircularTimerRing!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func run(_ sender: Any) {
+        
+        timerRing.startTimer(to: 10) { (state ) in
+            switch state {
+            case .finished:
+                print("finished")
+            case .continued(let time):
+                print("continued:\(time)")
+            case .paused(let time):
+                print("paused:\(time)")
+            }
+        }
+        
+    }
+    
+    @IBAction func stop(_ sender: Any) {
+        
+        timerRing.pauseTimer()
+    }
+    
 }
 
